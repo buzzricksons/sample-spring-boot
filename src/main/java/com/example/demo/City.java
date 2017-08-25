4a55b45fa8f8f18a0f319f4b4e1ee122fc4fd816
COMMERCE21\luke <luke@commerce21.co.jp>
2017-08-25 17:52:57 +0900

    edit lombok

--- src/main/java/com/example/demo/City.java
+++ src/main/java/com/example/demo/City.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import lombok.*;
 
 @Getter
+@ToString
 @RequiredArgsConstructor
 public class City {
+    @NonNull
     private Long id;
+    @NonNull
     private String name;
+    @NonNull
     private String state;
+    @NonNull
     private String country;
-
 }

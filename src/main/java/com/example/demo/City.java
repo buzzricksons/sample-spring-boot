0b74900ed33405c1aff1b98793b88e80ec49b1e1
COMMERCE21\luke <luke@commerce21.co.jp>
2017-08-29 18:55:02 +0900

    test lombok setting

--- src/main/java/com/example/demo/City.java
+++ src/main/java/com/example/demo/City.java
@@ -2,9 +2,9 @@ package com.example.demo;
 
 import lombok.*;
 
-@Getter
-@ToString
-@RequiredArgsConstructor
+//@Getter
+//@ToString
+//@RequiredArgsConstructor
 public class City {
     @NonNull
     private int id;
@@ -14,4 +14,43 @@ public class City {
     private String state;
     @NonNull
     private String country;
+
+    public City(int id, String name, String state, String country) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+        this.country = country;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 }

3a490c802f3a3a0cac448887755298303c64b032
Hyungcheol Kim <hcgoon@gmail.com>
2017-12-06 19:10:39 +0900

    edit package name

--- src/main/groovy/com/example/demo/mapper/CrudExampleMapperGroovy.groovy
+++ src/main/groovy/com/example/demo/mapper/CrudExampleMapperGroovy.groovy
@@ -7,8 +7,6 @@ import org.apache.ibatis.annotations.Mapper
 import org.apache.ibatis.annotations.Param
 import org.apache.ibatis.annotations.Select
 import org.apache.ibatis.annotations.Update
-import org.springframework.context.annotation.Bean
-
 
 @Mapper
 interface CrudExampleMapperGroovy {
